28af441eedf7366f2871af5305d472138282e1c9
Adityo Arya Putra <adityo.arya.ft21@mail.umy.ac.id>
2022-11-07 16:57:30 +0700

    add a node in the list

--- single_linked_list/Program.cs
+++ single_linked_list/Program.cs
@@ -21,8 +21,18 @@ namespace single_linked_list
         {
             START = null;
         }
-        public void addNote()
+        public void addNote() //add a node in the list
         {
+            int rollNo;
+            string nm;
+            Console.Write("\nEnter the roll Number of the student : ");
+            rollNo = Convert.ToInt32(Console.ReadLine);
+            Console.Write("\nEnter the roll Name of the student : ");
+            nm = Console.ReadLine();
+            Node newnode = new Node();
+            newnode.rollNumber = rollNo;
+            newnode.name = nm;
+
 
         }
     }
