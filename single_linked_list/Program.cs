bcf5dc6fa4c5227c42416628f386c48efb6f72f2
Adityo Arya Putra <adityo.arya.ft21@mail.umy.ac.id>
2022-11-07 16:47:03 +0700

    create class Node and class List

--- single_linked_list/Program.cs
+++ single_linked_list/Program.cs
@@ -6,6 +6,15 @@ using System.Threading.Tasks;
 
 namespace single_linked_list
 {
+    class Node
+    {
+        
+    }
+
+    class List
+    {
+
+    }
     internal class Program
     {
         static void Main(string[] args)
